76537bd40c9cda7bbca389b1cda823839f7cf11a
Luke Petschauer <lpetschauer@gmail.com>
2014-08-16 14:57:27 +0800

    solved, but hacky and ugly
    need to fix up the string part at the end
    
    i imagine i could also use a lambda function to smooth things over,
    too. have to read about that.

--- morse_clock.py
+++ morse_clock.py
@@ -3,28 +3,68 @@
 
 def checkio(time_string):
     # first, split up the time_string into hours, minutes, seconds
-    tt = time_string.split(':')
-    hours, min, sec = int(tt[0]), int(tt[1]), int(tt[2])
-
-    # now, make sure each value has a zero following it.
-    # this is really hacky right now. need to improve it.
-    if len(str(hours))<2:
-        hours *= 10
-    if len(str(min)) < 2:
-        min *= 10
-    if len(str(sec)) < 2:
-        sec *= 10
+    tt = time_string.split(':')  # split it up into h/m/s
+    tt = [num.zfill(2) for num in tt]  # make sure h/m/s are all two digit
+    hours, min, sec = tt[0], tt[1], tt[2]  # got 'em.
 
     # start doing conversion to binary for each.
-    # remember that python's bin() spits it out as '0b...'
-
+    h1, h2 = bin(int(hours[0])), bin(int(hours[1]))  # get it in binary
+    h1, h2 = str(h1[2:]).zfill(2), str(h2[2:]).zfill(4)  # turn into str and strip the '0b'
+    m1, m2 = bin(int(min[0])), bin(int(min[1]))
+    m1, m2 = str(m1[2:]).zfill(3), str(m2[2:]).zfill(4)
+    s1, s2 = bin(int(sec[0])), bin(int(sec[1]))
+    s1, s2 = str(s1[2:]).zfill(3), str(s2[2:]).zfill(4)
 
     # now convert binary into Morse Code.
     # 0=. and 1=-
+    final = ""
+    hours1 = ''
+    hours2 = ''
+    min1 = ''
+    min2 = ''
+    sec1 = ''
+    sec2 = ''
 
+    for num in h1:
+        if num == '1':
+            hours1 += '-'
+        else:
+            hours1 += '.'
+
+    for num in h2:
+        if num == '1':
+            hours2 += '-'
+        else:
+            hours2 += '.'
+
+    for num in m1:
+        if num == '1':
+            min1 += '-'
+        else:
+            min1 += '.'
+
+    for num in m2:
+        if num == '1':
+            min2 += '-'
+        else:
+            min2 += '.'
+
+    for num in s1:
+        if num == '1':
+            sec1 += '-'
+        else:
+            sec1 += '.'
+
+    for num in s2:
+        if num == '1':
+            sec2 += '-'
+        else:
+            sec2 += '.'
+
+    return hours1, hours2, ':', min1, min2, ":", sec1, sec2
 
     #return the solution
-    return ".- .... : .-- .--- : -.. -..-"
+    # return final
 
 if __name__ == '__main__':
     assert checkio("10:37:49") == ".- .... : .-- .--- : -.. -..-", "First Test"
