417f70b60d3faafd4da555f6db4347038318ab72
Luke Petschauer <lpetschauer@gmail.com>
2014-08-17 11:28:31 +0800

    substantially improved
    used str.replace()
    combined a bunch of lines of code
    made final be much nicer

--- morse_clock.py
+++ morse_clock.py
@@ -8,63 +8,19 @@ def checkio(time_string):
     hours, min, sec = tt[0], tt[1], tt[2]  # got 'em.
 
     # start doing conversion to binary for each.
-    h1, h2 = bin(int(hours[0])), bin(int(hours[1]))  # get it in binary
-    h1, h2 = str(h1[2:]).zfill(2), str(h2[2:]).zfill(4)  # turn into str and strip the '0b'
-    m1, m2 = bin(int(min[0])), bin(int(min[1]))
-    m1, m2 = str(m1[2:]).zfill(3), str(m2[2:]).zfill(4)
-    s1, s2 = bin(int(sec[0])), bin(int(sec[1]))
-    s1, s2 = str(s1[2:]).zfill(3), str(s2[2:]).zfill(4)
+    h1, h2 = str(bin(int(hours[0])))[2:].zfill(2),
+             str(bin(int(hours[1])))[2:].zfill(4)
+    m1, m2 = str(bin(int(min[0])))[2:].zfill(3), str(bin(int(min[1])))[2:].zfill(4)
+    s1, s2 = str(bin(int(sec[0])))[2:].zfill(3), str(bin(int(sec[1])))[2:].zfill(4)
 
-    # now convert binary into Morse Code.
-    # 0=. and 1=-
-    final = ""
-    hours1 = ''
-    hours2 = ''
-    min1 = ''
-    min2 = ''
-    sec1 = ''
-    sec2 = ''
-
-    for num in h1:
-        if num == '1':
-            hours1 += '-'
-        else:
-            hours1 += '.'
-
-    for num in h2:
-        if num == '1':
-            hours2 += '-'
-        else:
-            hours2 += '.'
-
-    for num in m1:
-        if num == '1':
-            min1 += '-'
-        else:
-            min1 += '.'
-
-    for num in m2:
-        if num == '1':
-            min2 += '-'
-        else:
-            min2 += '.'
-
-    for num in s1:
-        if num == '1':
-            sec1 += '-'
-        else:
-            sec1 += '.'
-
-    for num in s2:
-        if num == '1':
-            sec2 += '-'
-        else:
-            sec2 += '.'
-
-    return hours1, hours2, ':', min1, min2, ":", sec1, sec2
+    # # now convert binary into Morse Code.
+    # # 0=. and 1=-
+    final = h1 + " " + h2 + ' : ' + m1 + ' ' + m2 + ' : ' + s1 + ' ' + s2
+    final = final.replace('0', '.')
+    final = final.replace('1', '-')
 
     #return the solution
-    # return final
+    return final
 
 if __name__ == '__main__':
     assert checkio("10:37:49") == ".- .... : .-- .--- : -.. -..-", "First Test"
